40ae5e778b8cb7630090ded86699334b6f87d582
Ben Grass <bvgrass@outlook.com>
2023-10-12 13:29:01 -0400

    responds with 404 page if request isn't to index

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,19 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let request: Vec<_> = BufReader::new(&mut stream)
-        .lines().map(|r| r.unwrap())
-        .take_while(|l| !l.is_empty()).collect();
+    let request= BufReader::new(&mut stream)
+        .lines().next().unwrap().unwrap();
 
-    let response_status = "HTTP/1.1 200 OK";
-    let html_contents = fs::read_to_string("hello.html").unwrap();
+    let (status, path) = if request == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let html_contents = fs::read_to_string(path).unwrap();
     let content_len = html_contents.len();
     // write response header status\r\nContent-Length: len\r\n\r\nContent
     let response =
-        format!("{response_status}\r\nContent-Length: {content_len}\r\n\r\n{html_contents}");
+        format!("{status}\r\nContent-Length: {content_len}\r\n\r\n{html_contents}");
     // write response
     stream.write_all(response.as_bytes()).unwrap();
 }
