c7e95f086bbbd7e7ed1ec007710ba77d795f4997
Ben Grass <bvgrass@outlook.com>
2023-10-12 17:22:11 -0400

    implement sleep

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use std::{
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-    fs
-};
+use std::{io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, fs, thread, time::Duration};
 use hello_server::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -18,11 +14,15 @@ fn handle_connection(mut stream: TcpStream) {
     let request= BufReader::new(&mut stream)
         .lines().next().unwrap().unwrap();
 
-    let (status, path) = if request == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status, path) = match &request[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
+
     let html_contents = fs::read_to_string(path).unwrap();
     let content_len = html_contents.len();
     // write response header status\r\nContent-Length: len\r\n\r\nContent
