6c2014cf8180a03d2a830c8d5387ed5615a5a422
Ben Grass <bvgrass@outlook.com>
2023-10-12 16:22:41 -0400

    structure multithreaded server

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,34 @@
-pub struct ThreadPool;
+use std::thread;
+pub struct ThreadPool {
+    threads: Vec<WorkerThread>
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
-        Self {}
+        let mut threads = Vec::with_capacity(size);
+        for id in 0..size {
+            threads.push(WorkerThread::new(id));
+        }
+
+        Self {threads}
     }
 
     pub fn execute<F>(&self, f: F)
         where F: FnOnce() + Send + 'static {
 
+    }
+}
+
+struct WorkerThread {
+    id: usize,
+    thread: thread::JoinHandle<()>
+}
+
+impl WorkerThread {
+    pub fn new(id: usize) -> Self {
+        let thread = thread::spawn(|| {});
+        Self { id, thread }
     }
 }
 
