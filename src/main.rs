7a6737d5359d50b074ecc87f273ede39b2d45b75
Ben Grass <bvgrass@outlook.com>
2023-10-12 13:14:21 -0400

    write sample html as response

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,11 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|l| !l.is_empty()).collect();
 
     let response_status = "HTTP/1.1 200 OK";
+    let html_contents = fs::read_to_string("hello.html").unwrap();
+    let content_len = html_contents.len();
     // write response header status\r\nContent-Length: len\r\n\r\nContent
+    let response =
+        format!("{response_status}\r\nContent-Length: {content_len}\r\n\r\n{html_contents}");
     // write response
-
+    stream.write_all(response.as_bytes()).unwrap();
 }
