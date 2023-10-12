76cf7e636e3887dd409a83897223ac6e4d322a9d
Ben Grass <bvgrass@outlook.com>
2023-10-12 17:12:54 -0400

    implement threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,32 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread
+};
 pub struct ThreadPool {
-    threads: Vec<WorkerThread>
+    workers: Vec<WorkerThread>,
+    sender: mpsc::Sender<Job>
 }
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
 impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
-        let mut threads = Vec::with_capacity(size);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
         for id in 0..size {
-            threads.push(WorkerThread::new(id));
+            workers.push(WorkerThread::new(id, receiver.clone()));
         }
 
-        Self {threads}
+        Self { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
         where F: FnOnce() + Send + 'static {
 
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -26,8 +36,12 @@ struct WorkerThread {
 }
 
 impl WorkerThread {
-    pub fn new(id: usize) -> Self {
-        let thread = thread::spawn(|| {});
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {id} got a job; executing.");
+            job();
+        });
         Self { id, thread }
     }
 }
