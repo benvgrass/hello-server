c82f54e7f3b75b825df9e412ceb5d7867deda394
Ben Grass <bvgrass@outlook.com>
2023-10-14 15:58:35 -0400

    no forced shutdown

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use hello_server::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let thread_pool = ThreadPool::new(4); // setting thread pool size as 4 initially
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
         thread_pool.execute(|| {
             handle_connection(stream);
